71539dcaaf31b3f2b7754658f21895002aa4dcaa
106061144 <denny.iamw5u@gmail.com>
2021-04-01 02:13:27 -0700

    add main.cpp

---
+++ LP_filter_selection/main.cpp
@@ -0,0 +1,148 @@
+#include "mbed.h"
+#include "uLCD_4DGL.h"
+
+uLCD_4DGL uLCD(D1, D0, D2);
+DigitalIn Up_button(D3);
+DigitalIn Down_button(D6);
+DigitalIn Confirm_button(D9);
+AnalogOut Aout(D7);
+DigitalOut led(LED1);
+DigitalOut flash(LED2);
+
+int mode=1;
+void situation1()
+{
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.color(BLUE);
+    uLCD.printf("\nPlease select from the below options\n");   
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.textbackground_color(WHITE);
+    uLCD.printf("\n  first mode");
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.textbackground_color(BLACK);
+    uLCD.printf("\n  second mode\n  third mode");
+    ThisThread::sleep_for(1s);
+}
+void situation2()
+{
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.color(BLUE);
+    uLCD.printf("\nPlease select from the below options\n");    
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.textbackground_color(BLACK);
+    uLCD.printf("\n  first mode");
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.textbackground_color(WHITE);
+    uLCD.printf("\n  second mode");
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.textbackground_color(BLACK);            
+    uLCD.printf("\n  third mode");
+    ThisThread::sleep_for(1s);
+}
+void situation3()
+{
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.color(BLUE);
+    uLCD.printf("\nPlease select from the below options\n");   
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.textbackground_color(BLACK);
+    uLCD.printf("\n  first mode\n  second mode");
+    uLCD.text_width(1); 
+    uLCD.text_height(1);
+    uLCD.textbackground_color(WHITE);
+    uLCD.printf("\n  third mode");
+    ThisThread::sleep_for(1s);
+}
+
+void wave(){
+    if (mode==1){// frequency=0.1
+        while (1) {
+            led = !led;
+            for (float i = 0.0; i < 1.0; i += 1.0/300.0) {
+                Aout = i;
+                ThisThread::sleep_for(1ms/1);
+            }
+            for (float j = 1.0; j > 0.0; j -= 1.0f/700.0){
+                Aout = j;
+                ThisThread::sleep_for(1ms/1);
+            }
+        } 
+    } else if(mode==2) {// frequency=1
+        while (1) {
+            flash = !flash;
+            for (float i = 0.0; i <= 1.0; i += 1.0/300.0) {
+                Aout = i;
+                ThisThread::sleep_for(1ms/10);
+            }
+            for (float j = 1.0; j >= 0.0; j -= 1.0f/700.0){
+                Aout = j;
+                ThisThread::sleep_for(1ms/10);
+            }
+        }
+    } else {
+        while (1) {// frequency=10
+            for (float i = 0.0; i <= 1.0; i += 1.0/300.0) {
+                Aout = i;
+                ThisThread::sleep_for(1ms/100);
+            }
+            for (float j = 1.0; j >= 0.0; j -= 1.0f/700.0){
+                Aout = j;
+                ThisThread::sleep_for(1ms/100);
+            }
+        }
+    }
+}
+int main()
+{
+    //button.rise(&flip);
+    situation1();
+    Up_button.mode(PullNone);
+    Down_button.mode(PullNone);
+    Confirm_button.mode(PullNone);
+    
+    while (1)
+    {
+        ThisThread::sleep_for(250ms);
+        if (mode==1){  
+            if (Confirm_button) {
+                wave();
+            }      
+            if (Down_button){
+                uLCD.cls();
+                situation2();
+                mode=2;
+            }
+        } else if (mode==2){ 
+            if (Confirm_button) {
+                wave();
+            }         
+            if (Up_button){
+                uLCD.cls();
+                situation1();
+                mode=1;
+            } else if (Down_button) {
+                uLCD.cls();
+                situation3();
+                mode=3;
+            }
+        } else {
+            if (Confirm_button) {
+                wave();
+            }  
+            if (Up_button){
+                uLCD.cls();
+                situation2();
+                mode=2;
+            } 
+        }
+    }
+}
